ae2f2a18a7d12cf2cf65a4756a719286093289ab
Melody Yang <melody870116@hotmail.com>
2018-02-25 00:37:36 -0800

    Finish recursion

--- source/bases.py
+++ source/bases.py
@@ -17,13 +17,41 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
 
+    decimal = 0
+    index = 0
+    # Decode digits from binary (base 2)
+    # if base == 2:
+    #     for digit in reversed(digits):
+    #         index += 1
+    #         decimal += (2 ** (index - 1)) * int(digit)
+    #
+    # # Decode digits from hexadecimal (base 16)
+    # if base == 16:
+    #     for digit in reversed(digits):
+    #         index += 1
+    #         if digit.islower():
+    #             decimal += (base ** (index - 1)) * (string.ascii_lowercase.index(digit) + 10)
+    #             print(decimal)
+    #         elif digit.isupper():
+    #             decimal += (base ** (index - 1)) * (string.ascii_uppercase.index(digit) + 10)
+    #         else:
+    #             decimal += (base ** (index - 1)) * int(digit)
+
+
+    # Decode digits from any base (2 up to 36)
+    if base >= 2 or base <= 36:
+        for digit in reversed(digits):
+            index += 1
+            if digit.islower():
+                decimal += (base ** (index - 1)) * (string.ascii_lowercase.index(digit) + 10)
+                print(decimal)
+            elif digit.isupper():
+                decimal += (base ** (index - 1)) * (string.ascii_uppercase.index(digit) + 10)
+            else:
+                decimal += (base ** (index - 1)) * int(digit)
+
+    return decimal
 
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
@@ -34,13 +62,38 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
+    # Encode number in binary (base 2)
+    encoded_num = ""
+    remainder = 0
 
+    # if base == 2:
+    #     while number != 0:
+    #         remainder = number % 2
+    #         encoded_num += str(remainder)
+    #         number = int(number/2)
+    #
+    # # Encode number in hexadecimal (base 16)
+    # if base == 16:
+    #     while number != 0:
+    #         remainder = number % 16
+    #         # print(remainder)
+    #         if remainder >= 10:
+    #             remainder = string.ascii_lowercase[remainder - 10]
+    #             print(remainder)
+    #         else:
+    #             encoded_num += str(remainder)
+    #             number = int(number/16)
+
+    # Encode number in any base (2 up to 36)
+    if base >= 2 or base <= 36:
+        while number != 0:
+            remainder = number % base
+            if remainder >= 10:
+                remainder = string.ascii_lowercase[remainder - 10]
+            encoded_num += str(remainder)
+            number = int(number/base)
+    # print(encoded_num[::-1])
+    return encoded_num[::-1]
 
 def convert(digits, base1, base2):
     """Convert given digits in base1 to digits in base2.
@@ -51,14 +104,26 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+
+    decimal = decode(digits, base1)
+    # Convert digits from base 2 to base 16 (and vice versa)
+    # base16 = encode(decimal, 16)
+    # decimal2 = decode(base16, 16)
+    # base2 = encode(decimal2, 2)
+
+    # Convert digits from base 2 to base 10 (and vice versa)
+    # base10 = encode(decimal, 10)
+    # decimal3 = decode(base10, 10)
+    # base2_2 = encode(decimal3, 2)
+
+    # Convert digits from base 10 to base 16 (and vice versa)
+    # base10_2 = encode(decimal, 16)
+    # decimal4= decode(base10_2, 16)
+    # base2_3 = encode(decimal4, 10)
+
+    # Convert digits from any base to any base (2 up to 36)
+    result = encode(decimal, base2)
+    return result
 
 
 def main():
--- source/recursion.py
+++ source/recursion.py
@@ -8,16 +8,18 @@ def factorial(n):
         raise ValueError('factorial is undefined for n = {}'.format(n))
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return factorial_iterative(n)
-    return factorial_recursive(n)
+
+    return factorial_iterative(n)
+
+    # return factorial_recursive(n)
 
 
 def factorial_iterative(n):
-    # TODO: implement the factorial function iteratively here
-    pass
-    # once implemented, change factorial (above) to call factorial_iterative
-    # to verify that your iterative implementation passes all tests
-
+    product = n
+    while n != 1:
+        product *= (n-1)
+        n -= 1
+    return product
 
 def factorial_recursive(n):
     # check if n is one of the base cases
@@ -38,6 +40,7 @@ def main():
         print('factorial({}) => {}'.format(num, result))
     else:
         print('Usage: {} number'.format(sys.argv[0]))
+    # print(factorial(5))
 
 
 if __name__ == '__main__':
