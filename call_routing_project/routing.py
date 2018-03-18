359f708e7846226e600b6d846e27b7c49da6578e
Melody Yang <melody870116@hotmail.com>
2018-03-17 22:45:39 -0700

    Complete scenario two

--- call_routing_project/routing.py
+++ call_routing_project/routing.py
@@ -2,11 +2,12 @@ import time
 
 class Routing:
 
-    def __init__(self, file_name):
-        self.pricing = self.read_costs_file(file_name)
+    def __init__(self, cost_file, number_file=None):
+        self.pricing = self.read_cost_file(cost_file)
         self.matches = {}
+        self.phone_nums = self.read_number_file(number_file)
 
-    def read_costs_file(self, file_name):
+    def read_cost_file(self, file_name):
         opened_file = open('datafiles/{}'.format(file_name))
         file_lines = opened_file.read().splitlines()
         pricing = {}
@@ -14,8 +15,13 @@ class Routing:
         for line in file_lines:
             split = line.split(',')
             pricing[split[0]] = split[1]
-        self.pricing = pricing
-        return self.pricing
+        return pricing
+
+    def read_number_file(self, file_name):
+        opened_file = open('datafiles/{}'.format(file_name))
+        file_lines = opened_file.read().splitlines()
+        return file_lines
+
 
     def find_matches(self, number):
         matches = {}
@@ -24,7 +30,6 @@ class Routing:
             if number.startswith(prefix):
                 matches[prefix] = self.pricing[prefix]
         self.matches = matches
-        print(self.matches)
         return self.matches
 
     def find_best_match(self, number):
@@ -37,22 +42,33 @@ class Routing:
                 if len(m) > str_len:
                     str_len = len(m)
                     match = self.matches[m]
-            self.record_result(number, match)
-        else:
-            self.record_result(number, self.matches[number])
+        elif len(self.matches) == 1:
+            match = list(self.matches.values())[0]
         return match
 
-    def record_result(self, number, price):
-        result_file = open("result.txt", "a")
-        result_file.write("{}, {}\n".format(number, price))
+    def record_result(self, number, match, N):
+        result_file = open("route-costs-{}.txt".format(N), "a")
+        result_file.write("{}, {}\n".format(number, match))
+
+    def find_multi_numbers(self):
+        if self.phone_nums is not None:
+            for num in self.phone_nums:
+                match = self.find_best_match(num)
+                self.record_result(num, match, 2)
+
+
 
 def main():
-    routing = Routing('route-costs-106000.txt')
+    number = '+19446855669'
+    routing = Routing('route-costs-106000.txt', 'phone-numbers-1000.txt')
     start_time = time.time()
-    routing.find_best_match('+449932173')
+    routing.find_multi_numbers()
+    # match = routing.find_best_match(number)
+    # print('match: ' + str(match))
     end_time = time.time()
-    print("Run time:", end_time - start_time)
+    print("Running time:", end_time - start_time)
 
+# Scenario 2 running time: 17.69
 
 if __name__ == "__main__":
     main()
