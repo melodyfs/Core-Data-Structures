e6f163864132e28f2ec447ad001e1258964db2b0
Melody Yang <melody870116@hotmail.com>
2018-03-19 16:42:10 -0700

    Change some code in routing

--- call_routing_project/routing.py
+++ call_routing_project/routing.py
@@ -4,8 +4,9 @@ class Routing:
 
     def __init__(self, cost_file, number_file=None):
         self.pricing = self.read_cost_file(cost_file)
-        self.matches = {}
-        self.phone_nums = self.read_number_file(number_file)
+        self.phone_nums = {}
+        if number_file is not None:
+            self.phone_nums = self.read_number_file(number_file)
 
     def read_cost_file(self, file_name):
         opened_file = open('datafiles/{}'.format(file_name))
@@ -22,53 +23,47 @@ class Routing:
         file_lines = opened_file.read().splitlines()
         return file_lines
 
-
-    def find_matches(self, number):
+    def find_all_matches(self, number):
         matches = {}
 
-        for prefix in self.pricing:
-            if number.startswith(prefix):
+        for prefix in self.pricing:  # ?? iterations
+            if number.startswith(prefix):  # O(??) running time
                 matches[prefix] = self.pricing[prefix]
-        self.matches = matches
-        return self.matches
+        return matches
 
     def find_best_match(self, number):
-        self.find_matches(number)
-        match = 0
-
-        if len(self.matches) > 1:
-            str_len = 0
-            for m in self.matches:
-                if len(m) > str_len:
-                    str_len = len(m)
-                    match = self.matches[m]
-        elif len(self.matches) == 1:
-            match = list(self.matches.values())[0]
+        matches = self.find_all_matches(number)
+        longest_key = max(matches, key=len)
+        match = matches[longest_key]
         return match
 
     def record_result(self, number, match, N):
         result_file = open("route-costs-{}.txt".format(N), "a")
         result_file.write("{}, {}\n".format(number, match))
 
-    def find_multi_numbers(self):
+    # Scenario 2: check route costs for multiple phone numbers
+    def find_multi_numbers(self, N):
         if self.phone_nums is not None:
             for num in self.phone_nums:
                 match = self.find_best_match(num)
-                self.record_result(num, match, 2)
+                self.record_result(num, match, N)
 
 
 
 def main():
     number = '+19446855669'
-    routing = Routing('route-costs-106000.txt', 'phone-numbers-1000.txt')
+    routing = Routing('route-costs-10000000.txt', 'phone-numbers-10000.txt')
+    # routing = Routing('route-costs-106000.txt')
     start_time = time.time()
-    routing.find_multi_numbers()
+    routing.find_multi_numbers(3)
     # match = routing.find_best_match(number)
     # print('match: ' + str(match))
     end_time = time.time()
     print("Running time:", end_time - start_time)
 
+# Scenario 1 running time: 0.017
 # Scenario 2 running time: 17.69
+# Scenario 3 running time:
 
 if __name__ == "__main__":
     main()
